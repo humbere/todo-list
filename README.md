ba96c245d5d29b44a4252dc65cb9b052b6ae0db8
Humberto Arzate Chavez <humberto.arzate@tsj.gob.mx>
2022-03-31 10:23:02 -0700

    Inicio de proyecto todo list

---
+++ README.md
@@ -0,0 +1,9 @@
+# Webpack componente todolist
+Ejemplo de uso de todo list
+Para usar este repositorio, deberán ejecutar el comando de ```npm install```
+
+Para construir este proyecto, deberán ejecutar el comando de ```npm run build```
+
+Para correr este proyecto, deberán ejecutar el comando de ```npm start```
+
+Pero esto lo veremos en la siguiente sección
---
+++ src/index.js
@@ -0,0 +1,20 @@
+import './styles.css';
+import { Todo, TodoList } from './classes/index';
+import { crearTodoHtml } from './js/componentes';
+
+export const todoList = new TodoList();
+
+// todoList.todos.forEach(todo => crearTodoHtml( todo ));
+todoList.todos.forEach(crearTodoHtml);
+
+todoList.todos[0].imprimirTodo();
+
+
+// const tarea = new Todo('Aprender javascript');
+// tarea.completado = true;
+
+// todoList.nuevoTodo( tarea );
+// console.log( todoList );
+
+// crearTodoHtml( tarea );
+
